55efa97a58e9c7b628ade753ea01f8ee2239c021
AlexBarreto <alexbarreto2020@hotmail.com>
2021-11-26 17:06:21 -0300

    LIKE utilizado para pesquisar dados no banco.

---
+++ aula-10/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    $pdo = new PDO('mysql:host=localhost;dbname=clientes','root','');
+
+
+        $sql = $pdo->prepare("INSERT INTO `clientes` LIKE '%g%'");//LIKE utilizado para pesquisar letra ou palavra chave no banco de dados com paramentros -> %iniciaCom, %temPeloMenos%, finalizaCom%
+
+        $sql->execute();
+
+        $emails = $sql->fetchAll();
+
+        print_r($emails);
+
+
+//INSERINDO INFORMAÇÃO AO BANCO DE DADOS MYSQL
+?>
